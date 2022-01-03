2c4bbfbc02bef21ed78486432b6e144e051b3900
Gurleen Singh <6935357+gurleen@users.noreply.github.com>
2022-01-03 15:02:28 -0500

    Added macro support

--- src/panels/Info.svelte
+++ src/panels/Info.svelte
@@ -1,11 +1,26 @@
 <script>
-    import { store } from '../store.js';
+    import { store, processMacro } from '../store.js';
+    import { isEmpty } from 'lodash-es';
 
+    let macro = ""
     let time = new Date()
 
     setInterval(() => {
         time = new Date()
     }, 500);
+
+    window.addEventListener('keydown', (e) => {
+        if(e.key === "Backspace") macro = macro.slice(0, -1)
+        if(e.key === "Enter" && !isEmpty(macro)) {
+            processMacro(macro)
+            console.log(macro) 
+            macro = ""
+            return 
+        }
+        if(macro.length > 3) return
+        if(!(e.key >>> 0 === parseFloat(e.key))) return
+        macro += e.key
+    })
 </script>
 
 <main>
@@ -13,6 +28,7 @@
     <h3>{$store.homeName} <span class="light">{$store.homeScore}</span> - <span class="light">{$store.awayScore}</span> {$store.awayName}</h3>
     <h4><span class="light">{$store.homeTO}</span> TIMEOUTS <span class="light">{$store.awayTO}</span></h4>
     <h3>{$store.period} - {$store.clock} - <span class="light">{$store.shotClock}</span></h3>
+    <h3>Macro: {macro || "<empty>"}</h3>
 </main>
 
 <style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
 import io from 'socket.io-client';
+import { findIndex } from "lodash";
 
 export const store = writable({
     homeScore: 0
@@ -23,3 +24,19 @@ export function casparEvent(action, item, layer) {
 
 socket.on("state-update", updateBulk)
 socket.emit("get-cache", updateBulk)
+
+
+export function processMacro(macro) {
+    let st = get(store)
+    switch(macro.charAt(0)) {
+        case "1":
+            var index = findIndex(st.stats.teams.H, (p) => p.uni == macro.slice(1))
+            if(index != -1) updateKey("homePlayerNum", index)
+            updateKey("scoreboard:toggle-home", null)
+            break;
+        case "2":
+            var index = findIndex(st.stats.teams.V, (p) => p.uni == macro.slice(1))
+            if(index != -1) updateKey("awayPlayerNum", index)
+            updateKey("scoreboard:toggle-away", null)
+    }
+}
